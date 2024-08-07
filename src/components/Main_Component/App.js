1ec342097425e4435eee41a7e9c44fb2e451f54f
Viktoria Patrina <v.patrina@innopolis.university>
2024-08-08 00:33:45 +0300

    first

---
+++ src/components/Main_Component/App.js
@@ -0,0 +1,10 @@
+import '../../css/App.css';
+import Main from '../../template/Main';
+
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
---
+++ src/template/Main.js
@@ -0,0 +1,12 @@
+import '../css/Main.css';
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="App">
+      <p>hhhhhhb</p>
+    </div>
+  );
+}
+
+export default Main;
