9fb6108c0c233c6d5a1ab284de9378c212b2a71b
Viktoria Patrina <v.patrina@innopolis.university>
2024-09-01 14:47:36 +0300

    fix sider

--- src/template/Carousel.js
+++ src/template/Carousel.js
@@ -1,5 +1,5 @@
 import '../css/Carousel.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -8,6 +8,7 @@ import imageFlower2 from '../image/flower2.png';
 import imageFlower3 from '../image/flower3.png';
 
 const Carousel = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
   const settings = {
     dots: true,
     infinite: true,
@@ -15,25 +16,39 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 3500,
     arrows: true,
+    beforeChange: (current, next) => setActiveSlide(next),
   };
 
   return (
     <div className="carousel-container">
       <Slider {...settings}>
         <div>
-          <img className="images" src={imageFlower1} alt="First flower" />
+          <img
+            className={`images ${activeSlide === 0 ? 'animate' : ''}`}
+            src={imageFlower1}
+            alt="First flower"
+          />
         </div>
         <div>
-          <img className="images" src={imageFlower2} alt="Second flower" />  
+          <img
+            className={`images ${activeSlide === 1 ? 'animate' : ''}`}
+            src={imageFlower2}
+            alt="Second flower"
+          />
         </div>
         <div>
-          <img className="images" src={imageFlower3} alt="Third flower" />
+          <img
+            className={`images ${activeSlide === 2 ? 'animate' : ''}`}
+            src={imageFlower3}
+            alt="Third flower"
+          />
         </div>
       </Slider>
     </div>
   );
 };
+
 
 export default Carousel;
